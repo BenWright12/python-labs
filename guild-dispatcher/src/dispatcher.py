58fade3263fd0f61ab491b0f389f40c4415b0cd3
Ben Wright <165907466+BenWright12@users.noreply.github.com>
2026-08-14 16:49:30 +0000

    Added two more adventurers while setting up the main code

--- guild-dispatcher/src/dispatcher.py
+++ guild-dispatcher/src/dispatcher.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
 
+# imported both json files to pull data from
 file_path1 = Path(__file__).parent.parent / "data" / "adventurers.json"
 file_path2 = Path(__file__).parent.parent / "data" / "quests.json"
 
@@ -10,5 +11,20 @@ with open(file_path1, "r") as file:
 with open(file_path2, "r") as file:
     quests = json.load(file)
 
-print(type(adventurers))
-print(type(quests))
+
+# Example - for loop in a dictionary
+# for name, stats in adventurers.items():
+    # 'name' is the key (e.g., "Borg")
+    # 'stats' is the inner dictionary
+#    char_class = stats["class"]
+#    print(f"{name} is a {char_class}")
+
+# Example - for loop in lists
+# for quest in quests:
+    # must loop through the list of lists
+    # then variable the section you want to target to loop through 
+#    target_day = quest["target_day"]
+#    name = quest["quest_title"]
+#    if target_day == "Tuesday":
+#        print(f"{name} is on a {target_day}")
+
